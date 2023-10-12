9f42a1655b537a49d956bcc9eab8e99d154f4c3b
MuneebHassanSiddiqui <129599243+MuneebHassanSiddiqui@users.noreply.github.com>
2023-10-12 10:57:19 -0700

    PHP TO SQL

--- ASSIGNMENTS/GET $ POST METHOD/index.php
+++ ASSIGNMENTS/GET $ POST METHOD/index.php
@@ -29,8 +29,6 @@ if(isset($_POST["fn"])){
 }
 
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -39,7 +37,7 @@ if(isset($_POST["fn"])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <title>Document</title>
+    <title>MHS</title>
 </head>
 <body>
 
@@ -68,8 +66,6 @@ if(isset($_POST["fn"])){
 		<li></li>
 	</ul>
 </div>
-
-
 
 <script src="script.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
